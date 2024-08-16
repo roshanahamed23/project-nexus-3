f0e3d05569517ffb79765c7bbb6a6d886d5f7f0a
roshan ahamed <roshanahamed9600@gmail.com>
2024-08-16 19:31:33 +0530

    hero updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ let array = [
     icon: pool1,
   },
   {
-    text: "We build software so smooth, even I can't make it look bad... and trust me, I've tried!",
+    text: `"We build software so smooth, even I can't make it look bad... and trust me, I've tried!"`,
     icon: pool2,
   },
   {
@@ -29,32 +29,30 @@ const Hero = () => {
   }, []);
 
   return (
-    <div className="h-screen">
+    <div className="h-screen relative">
       <div className="separate"></div>
 
-      <div className="bg-red p-4 flex flex-row gap-4">
-
+      <div className="absolute inset-0 bg-red p-4 flex flex-row gap-4">
         <div className="bg-black w-1/2 max-md:w-full h-[96vh] flex justify-center items-center flex-col gap-10">
-          <h2 className="text-red text-6xl font-black max-md:text-5xl text-center p-6 max-md:max-w-2xl">
+          <h2 className="text-red text-6xl font-black max-md:text-3xl text-center p-6 max-md:max-w-2xl">
             Building Awesome Software with a{" "}
             <span className="text-white">Twist</span>
           </h2>
 
-          <div className=" md:hidden flex justify-center items-center w-2">
-          <Image
-            src={array[currentIndex].icon}
-            alt="pool"
-            priority={true}
-            className="max-w-xl"
-          />
-        </div>
+          <div className=" md:hidden flex justify-center items-center">
+            <Image
+              src={array[currentIndex].icon}
+              alt="pool"
+              priority={true}
+              className="w-28"
+            />
+          </div>
 
-
-          <h2 className="text-yellow-300 font-bebas text-4xl max-md:text-3xl p-6 shadow-lg shadow-red max-md:max-w-xl border-dashed border-[3px]">
+          <h2 className="text-yellow-300 font-bebas text-4xl max-md:text-2xl p-6 shadow-lg shadow-red max-md:max-w-lg border-dashed border-[3px]">
             {array[currentIndex].text}
           </h2>
           <div className="flex items-end">
-            <button className="text-black bg-red font-bold text-xl bg-red-500 rounded-lg px-6 py-3 hover:bg-red hover:text-white transition-transform duration-1000 ease-in-out transform hover:scale-110">
+            <button className="text-black bg-red font-bold text-xl max-md:text-md bg-red-500 rounded-lg px-6 py-3 hover:bg-red hover:text-white transition-transform duration-1000 ease-in-out transform hover:scale-110">
               Pool us
             </button>
           </div>
